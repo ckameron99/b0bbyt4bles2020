3614d009a72693e11e142297b377f0317512d0aa
Cameron Mackenzie <ckameron99@gmail.com>
2019-10-17 21:26:51 +0100

    initial setup.py created

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3.8
+alias python=python3.8
+alias python3=python3.8
+
+pip install pillow
+pip install matplotlib
