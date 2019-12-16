9062899379926546d37bf922448e9a9dd6cf5ae8
Cameron Mackenzie <ckameron99@gmail.com>
2019-12-16 13:05:51 +0000

    added autostart for setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,13 @@ sudo apt-get upgrade
 sudo apt-get install python3.8
 alias python=python3.8
 alias python3=python3.8
+alias pip=pip3
 
 pip install pillow
 pip install matplotlib
 pip install numpy
 pip install lidar
+
+sudo cp robot.service /etc/systemd/system/robot.service
+sudo systemctl start robot.service
+sudo systemctl enable robot.service
