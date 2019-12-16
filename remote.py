758e3fd08a3fbcca52dc0bd669cd36ca887bf841
Cameron Mackenzie <ckameron99@gmail.com>
2019-12-16 14:43:55 +0000

    updated remote code

--- remote.py
+++ remote.py
@@ -4,52 +4,43 @@ def rc():
     maxSpeed=1
     while True:
         if robot.controller.getConnectionStatus():
-            if robot.controller.dpadUp():
+            if robot.controller.dPadUpButton():
                 maxSpeed=min(1,maxSpeed+0.1)
                 time.sleep(0.2)
-            elif robot.controller.dpadDown():
+            elif robot.controller.dPadDownButton():
                 maxSpeed=max(0.5,maxSpeed-0.1)
                 time.sleep(0.2)
-            if robot.controller.Y():
+            if robot.controller.yButton():
                 robot.reverse=robot.reverse^1
                 time.sleep(0.2)
 
-            if robot.controller.rightTrigger():
-                if robot.controller.leftX() < -0.2:
-                    robot.forward((max(maxSpeed,0.8)*robot.controller.rightTrigger()*abs(1-abs(robot.controller.leftX()))),(max(maxSpeed,0.8)*robot.controller.rightTrigger()))
-                elif robot.controller.leftX() > 0.2:
-                    robot.forward((max(maxSpeed,0.8)*robot.controller.rightTrigger()),(max(maxSpeed,0.8)*robot.controller.rightTrigger()*abs(1-abs(robot.controller.leftX()))))
+            if robot.controller.rightTriggerLocation():
+                if robot.controller.leftXButton() < -0.2:
+                    robot.forward((max(maxSpeed,0.8)*robot.controller.rightTriggerLocation()*abs(1-abs(robot.controller.leftXButton()))),(max(maxSpeed,0.8)*robot.controller.rightTriggerLocation()))
+                elif robot.controller.leftXButton() > 0.2:
+                    robot.forward((max(maxSpeed,0.8)*robot.controller.rightTriggerLocation()),(max(maxSpeed,0.8)*robot.controller.rightTriggerLocation()*abs(1-abs(robot.controller.leftXButton()))))
                 else:
-                    robot.forward((maxSpeed*robot.controller.rightTrigger()),(maxSpeed*robot.controller.rightTrigger()))
+                    robot.forward((maxSpeed*robot.controller.rightTriggerLocation()),(maxSpeed*robot.controller.rightTriggerLocation()))
 
-            elif robot.controller.leftTrigger():
-                if robot.controller.leftX() > 0.2:
-                    robot.forward((max(maxSpeed,0.8)*-robot.controller.leftTrigger()*abs(1-abs(robot.controller.leftX()))),(max(maxSpeed,0.8)*-robot.controller.leftTrigger()))
-                elif robot.controller.leftX() < -0.2:
-                    robot.forward((max(maxSpeed,0.8)*-robot.controller.leftTrigger()),(max(maxSpeed,0.8)*-robot.controller.leftTrigger()*abs(1-abs(robot.controller.leftX()))))
+            elif robot.controller.leftTriggerLocation():
+                if robot.controller.leftXButton() > 0.2:
+                    robot.forward((max(maxSpeed,0.8)*-robot.controller.leftTriggerLocation()*abs(1-abs(robot.controller.leftXButton()))),(max(maxSpeed,0.8)*-robot.controller.leftTriggerLocation()))
+                elif robot.controller.leftXButton() < -0.2:
+                    robot.forward((max(maxSpeed,0.8)*-robot.controller.leftTriggerLocation()),(max(maxSpeed,0.8)*-robot.controller.leftTriggerLocation()*abs(1-abs(robot.controller.leftXButton()))))
                 else:
-                    robot.forward((maxSpeed*-robot.controller.leftTrigger()),(maxSpeed*-robot.controller.leftTrigger()))
-            elif robot.controller.leftBumper():
+                    robot.forward((maxSpeed*-robot.controller.leftTriggerLocation()),(maxSpeed*-robot.controller.leftTriggerLocation()))
+            elif robot.controller.leftBumperButton():
                 robot.turn((-0.9))
-            elif robot.controller.rightBumper():
+            elif robot.controller.rightBumperButtonr():
                 robot.turn((0.9))
             else:
-                if abs(robot.controller.leftY()-robot.controller.rightY())<0.5:
-                    robot.forward((maxSpeed*robot.controller.leftY()),(maxSpeed*robot.controller.rightY()))
+                if abs(robot.controller.leftY()-robot.controller.rightYButton())<0.5:
+                    robot.forward((maxSpeed*robot.controller.leftYButton()),(maxSpeed*robot.controller.rightYButton()))
                 else:
-                    robot.forward((max(maxSpeed,0.8)*robot.controller.leftY()),(max(maxSpeed,0.8)*robot.controller.rightY()))
+                    robot.forward((max(maxSpeed,0.8)*robot.controller.leftYButton()),(max(maxSpeed,0.8)*robot.controller.rightYButton()))
         else:
             robot.stop()
 
-        #print(robot.ultraFrontRight.distance())
-        a = robot.ultraFrontLeftSide.distance()
-        if a < 14:
-               pass
-               #print("Got 1")
-               #robot.stop()
-               #time.sleep(1)
-        print(a)
-
-        if robot.controller.X():
+        if robot.controller.xButton():
             robot.stop()
             break
