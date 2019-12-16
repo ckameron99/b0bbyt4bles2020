d22b9312dc7fe6f7d74f80874b71b40610331bb1
Cameron Mackenzie <ckameron99@gmail.com>
2019-12-16 14:18:38 +0000

    commented some code

--- robot.py
+++ robot.py
@@ -15,45 +15,45 @@ class Motion:
             rightSpeed=leftSpeed
         if not self.reverse:
 			self.robot.thunderborg.ZB.SetMotor1(speed1)
-			self.robot.thunderborg.ZB.SetMotor2(-speed2) #Remove /2
+			self.robot.thunderborg.ZB.SetMotor2(-speed2)
 			self.rightSpeed = speed2
 			self.leftSpeed = speed1
 		else:
 			self.robot.thunderborg.ZB.SetMotor1(-speed2)
-			self.robot.thunderborg.ZB.SetMotor2(speed1) #Remove /2
+			self.robot.thunderborg.ZB.SetMotor2(speed1)
 			self.rightSpeed = speed1
 			self.leftSpeed = speed2
 
-    def incrementSpeed(self,IncL,IncR):
-		#if self.rightSpeed + IncR > 1 or self.rightSpeed + IncR < -1 or self.leftSpeed + IncL > 1 or self.leftSpeed < -1:
-		#	raise Exception("Cant increment motors above 1 or below -1")
+    def incrementSpeed(self,IncL,IncR): #relativly change the motor speeds
 		self.rightSpeed += IncR
 		self.leftSpeed += IncL
 		self.forward(self.leftSpeed,self.rightSpeed)
 
     def stop(self):
-        self.softStop(0.5)
+        self.softStop(0.5) # makes the default stopping method the soft stop method to prevent flipping/damaging the robot, etc
 
     def softStop(self,timeToStop=0.5):
         DecL = (self.leftSpeed/100) * -1
 		DecR = (self.rightSpeed/100) * -1
-		for i in range(100):
+		for i in range(100): # slows down the speed to 0 within the specified time in increments of 100
 			self.incrementSpeed(DecL, DecR)
 			time.sleep(timeTo/100)
+
+
     def hardStop(self):
-        self.forward(0,0)
+        self.forward(0,0) # please try to use stop() or softStor() unless hardStop offers a competitive advantage
 
 
-    def toggleFront(self):
+    def toggleFront(self): # flips the front of the robot to the other end
         self.robot.front^=1
 
-    def changeFront(self,front):
+    def changeFront(self,front): # absolutly sets the front of the robot, can either take in a boolean variable, or a string which contains 'camera' or 'attachment'
         if "camera" in front.lower() or front==0:
             self.robot.front=0
         elif "attachment" in front.lower() or front==1:
             self.robot.front=1
 
-    def getFront(self):
+    def getFront(self): # returns a string representation of which side the robot is on, to access a boolean representation, use robot.front
         if self.robot.front:
             return "Camera-side"
         return "Attachment-side"
@@ -62,13 +62,14 @@ class Motion:
 class Controller:
     def __init__(self,robot):
         self.robot=robot
-        for i in range(100):
+        for i in range(100): # connecting is unreliable, so when initiating the controller it attempts up to 100 connections
             try:
                 self.xbox=xbox.Joystick()
                 return
             except:
                 time.sleep(0.5)
         raise RuntimeError("Unable to connect to XBox controller after 100 attempts")
+
     def close(self):
         self.xbox.close()
 
@@ -95,10 +96,11 @@ class Compass:
                 raise RuntimeError("Compass not found after 10 attempts")
             self.SetValues([240, 255, 216, 255, 240, 255, 222, 246, 167, 3, 91, 241, 254, 255, 254, 255, 255, 255, 232, 3, 225, 2]) # initial values to make calibration easier
             while True:
-				if self.Heading() != 0:
+				if self.getHeading() != 0:
 					break
 				print("Please shake to initialize")
-            print("Please place on a level surface for finnish initialization")
+                time.sleep(0.3)
+            print("Please place on a flat surface for finnish initialization")
             self.calibrate()
 			print("Initialization complete")
 			self.started = True
@@ -122,10 +124,10 @@ class Compass:
 
     def getCalibrationStatus(self):
 		sys, gyro, accel, mag = self.bno.get_calibration_status()
-		return True if min([sys,gyro,accel,mag]) == 3 else False
+		return True if min([sys,gyro,accel,mag]) == 3 else False # true if the robot is calibrated well
 
     def getCalibrationValues(self):
-        return self.bno.get_calibration_status()
+        return self.bno.get_calibration_status() # return how well calibrated each aspect of the compass is
 
     def setCalibrationValues(self,values):
 		for i in range(10):
@@ -146,6 +148,7 @@ class Thunderborg:
         self.robot=robot
         self.ZB=ThunderBorg3.ThunderBorg()
         self.initiate()
+
     def initiate(self):
         self.ZB.init()
 
@@ -175,6 +178,7 @@ class Robot: # ties all other classes together, and allows functions that use se
 		except:
 			pass
 		print("Processes Safely Stopped")
+        
     def stop(self):
         self.motion.forward(0)
 
